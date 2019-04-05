779df25cb2bb864c5015ddecca79e94606235e16
ruifshi <ruifshi@hotmail.com>
2019-04-04 22:31:40 -0500

    Add Dijkstra, Kruskals, and Prim's implementations

--- LeetCode/Kruskals.cpp
+++ LeetCode/Kruskals.cpp
@@ -5,6 +5,8 @@
 
 using namespace std;
 
+//https://www.programiz.com/dsa/kruskal-algorithm
+
 #define V 5 
 int parent[V];
 
@@ -57,6 +59,44 @@ void kruskalMST(int cost[][V])
 	printf("\n Minimum cost= %d \n", mincost);
 }
 
-// This can be optimized by using a priority queue using the weight as the comparator. pair<weight, pair<x, y>>
-// Then we just loop through the queue and pick the smallest weight that does not create a cycle in the graph
-//https://www.youtube.com/watch?v=7gD5liN3HCw
+/*
+int Graph::kruskalMST()
+{
+	int mst_wt = 0; // Initialize result
+
+	// Sort edges in increasing order on basis of cost
+	sort(edges.begin(), edges.end());
+
+	// Create disjoint sets
+	DisjointSets ds(V);
+
+	// Iterate through all sorted edges
+	vector< pair<int, iPair> >::iterator it;
+	for (it=edges.begin(); it!=edges.end(); it++)
+	{
+		int u = it->second.first;
+		int v = it->second.second;
+
+		int set_u = ds.find(u);
+		int set_v = ds.find(v);
+
+		// Check if the selected edge is creating
+		// a cycle or not (Cycle is created if u
+		// and v belong to same set)
+		if (set_u != set_v)
+		{
+			// Current edge will be in the MST
+			// so print it
+			cout << u << " - " << v << endl;
+
+			// Update MST weight
+			mst_wt += it->first;
+
+			// Merge two sets
+			ds.merge(set_u, set_v);
+		}
+	}
+
+	return mst_wt;
+}
+*/
--- LeetCode/Prims.cpp
+++ LeetCode/Prims.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 
 using namespace std;
+
+//https://www.programiz.com/dsa/prim-algorithm
+
 #define V 5 
 
 // Returns true if edge u-v is a valid edge to be 
@@ -55,4 +58,70 @@ void primMST(int cost[][V])
 	printf("\n Minimum cost= %d \n", mincost);
 }
 
-//https://www.youtube.com/watch?v=qGp37svIN9E
+/*
+// Prints shortest paths from src to all other vertices
+void Graph::primMST()
+{
+	// Create a priority queue to store vertices that
+	// are being preinMST. This is weird syntax in C++.
+	// Refer below link for details of this syntax
+	// http://geeksquiz.com/implement-min-heap-using-stl/
+	priority_queue< iPair, vector <iPair> , greater<iPair> > pq;
+
+	int src = 0; // Taking vertex 0 as source
+
+	// Create a vector for keys and initialize all
+	// keys as infinite (INF)
+	vector<int> key(V, INF);
+
+	// To store parent array which in turn store MST
+	vector<int> parent(V, -1);
+
+	// To keep track of vertices included in MST
+	vector<bool> inMST(V, false);
+
+	// Insert source itself in priority queue and initialize
+	// its key as 0.
+	pq.push(make_pair(0, src));
+	key[src] = 0;
+
+	// Looping till priority queue becomes empty 
+while (!pq.empty())
+{
+	// The first vertex in pair is the minimum key 
+	// vertex, extract it from priority queue. 
+	// vertex label is stored in second of pair (it 
+	// has to be done this way to keep the vertices 
+	// sorted key (key must be first item 
+	// in pair) 
+	int u = pq.top().second;
+	pq.pop();
+
+	inMST[u] = true;  // Include vertex in MST 
+
+	// 'i' is used to get all adjacent vertices of a vertex 
+	list< pair<int, int> >::iterator i;
+	for (i = adj[u].begin(); i != adj[u].end(); ++i)
+	{
+		// Get vertex label and weight of current adjacent 
+		// of u. 
+		int v = (*i).first;
+		int weight = (*i).second;
+
+		//  If v is not in MST and weight of (u,v) is smaller 
+		// than current key of v 
+		if (inMST[v] == false && key[v] > weight)
+		{
+			// Updating key of v 
+			key[v] = weight;
+			pq.push(make_pair(key[v], v));
+			parent[v] = u;
+		}
+	}
+}
+
+// Print edges of MST using parent array 
+for (int i = 1; i < V; ++i)
+	printf("%d - %d\n", parent[i], i);
+}
+*/
